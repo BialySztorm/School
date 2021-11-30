4af0ec94c8748488cc52f7727980b74e22a78a7a
Andrzej Manderla <andrzejmmm1@gmail.com>
2021-11-30 20:49:21 +0100

    Daria 018/02

---
+++ xampp/htdocs/szkola/Dazia/018/02.php
@@ -0,0 +1,16 @@
+<?php
+$file =  "parzystePrzez3.txt";
+$handle = fopen($file, "w");
+$tmp = "";
+for($i = 2; $i<=1000; $i+=2)
+{
+    if(($i%3) == 0){
+        $tmp .= "$i, ";
+    }
+}
+fputs($handle,$tmp);
+fclose($handle);
+
+$handle = fopen($file, "r");
+echo fread($handle,filesize($file));
+?>
