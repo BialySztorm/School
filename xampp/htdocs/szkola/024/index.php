cb84422e8ae44fb9b07a046b25fa6007857b7e42
Andrzej Manderla <andrzejmmm1@gmail.com>
2022-01-03 11:13:51 +0100

    twww 24

---
+++ xampp/htdocs/szkola/024/index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="AM.png" type="image/x-icon" />
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Login page</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        $connection = mysqli_connect("localhost", "root");
+        mysqli_select_db($connection, "baza");
+        if (isset($_COOKIE['username']) && mysqli_fetch_assoc(mysqli_query($connection,"SELECT count(tokens.token) AS isToken FROM tokens WHERE  tokens.token = '".$_COOKIE['username']."'"))['isToken'] !=0) {
+            header("Location: secret.php");
+        }
+        mysqli_close($connection);
+        ?>
+        <form action="secret.php" method="post">
+            <input type="email" placeholder="email" name="email">
+            <input type="password" placeholder="password" name="password">
+            <input class="button" type="submit" value="Login">
+        </form>
+    </div>
+</body>
+
+</html>
