46f97e344f9f617d6c814fde548685da95850457
Andrzej Manderla <andrzejmmm1@gmail.com>
2022-02-01 16:00:36 +0100

    ckz 15

--- Tw.WWW/ckz/15/skrypt.js
+++ Tw.WWW/ckz/15/skrypt.js
@@ -9,7 +9,7 @@ $(".game__buttons--btn").click(function () {
     var tmp = $(".game--counter")[0]
     if (!isNaN(tmp.innerHTML)) {
         var thisLen = $(this).children().length
-        var score = tmp - thisLen
+        var score = tmp.innerHTML - thisLen
         if (score == 0) {
             tmp.innerHTML = "Koniec"
         } else if (score < 0) {
