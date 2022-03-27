1bba9794579285e120754b57d714842a222c3b7b
BialySztorm <andrzejmmm1@gmail.com>
2022-03-28 00:34:54 +0200

    quick fix

--- Tw.WWW/Strony/29.Pliki/loaders/mongoose.js
+++ Tw.WWW/Strony/29.Pliki/loaders/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = "mongodb://mo24648_School:Swinia2002!@mongo.ct8.pl:27017/mo24648_School"
+const url = ""//paste here credentials
 
 mongoose.connect(url, {
     // some settings for better mongoose performance
